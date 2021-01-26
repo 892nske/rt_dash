0faabbc31cf22a9704e868a4b429eec6b8d238e0
892nske <tomo.kadokura@gmail.com>
2021-01-26 15:40:43 +0900

    he

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def create_pieChart(weight):
     }
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run_server(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
